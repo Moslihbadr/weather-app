3b1a4f3f13ffcd68b7e7e9b4fc07dfee93a3d702
Badr Moslih <113358493+Moslihbadr@users.noreply.github.com>
2023-03-14 00:43:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
-const APIKey = "dbdcf6becc4e448c01037923ba02671c"
-let btn = document.querySelector("button")
-let input = document.querySelector("input")
+// variables
+const APIKey = "dbdcf6becc4e448c01037923ba02671c";
+let btn = document.querySelector("button");
+let input = document.querySelector("input");
+let img = document.querySelector("img");
+let temp = document.getElementById("temp");
+let desc = document.getElementById("desc");
+let MinMax = document.getElementById("MinMax");
+let humidity = document.getElementById("humidity");
+let wind = document.getElementById("wind");
 
+
+
+// main function
 btn.addEventListener("click",()=>{
-  const API = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${APIKey}&units=metric`
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET",API,true)
-  xhr.send()
-  xhr.onreadystatechange = function(){
-    // console.log(this.responseText)
-    if(this.readyState == 4 && this.status == 200){
-      let data = JSON.parse(this.responseText);
-      console.log("this.responseText")
-      console.log(data)
-      document.body.append(data.main.temp)  
-      document.body.append(data.main.humidity)
-      document.body.append(data.wind.speed)
-      document.body.append(data.weather[0].main) // Ex:clouds
+  if(input.value == ""){
+    input.focus()
+  }else{
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${APIKey}&units=metric`;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET",API,true);
+    xhr.send();
+    xhr.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+        let weatherData = JSON.parse(this.responseText);
+        let card = document.querySelector(".card")
+        card.style.height = "550px";
+        let content = document.querySelector('.weather')
+        content.style.display = 'block';
+        document.getElementById("city").innerHTML = weatherData.name;
+        img.src = `./icons/${weatherData.weather[0].main}.png`;
+        temp.innerHTML = `${Math.round(weatherData.main.temp)}°c`;
+        desc.innerText = weatherData.weather[0].description;
+        MinMax.innerHTML = `<i class="fa-solid fa-temperature-arrow-down"></i> ${Math.round(weatherData.main.temp_min)}°c / <i class="fa-solid fa-temperature-arrow-up"></i> ${Math.round(weatherData.main.temp_max)}°c`;
+        humidity.innerHTML = `<i class="fa-solid fa-water"></i> ${weatherData.main.humidity}%`;
+        wind.innerHTML = `<i class="fa-solid fa-wind"></i> ${Math.round(weatherData.wind.speed)} km/h`;
+      }else if(this.readyState == 4 && this.status == 404){
+        window.alert("Invalid City Name")
+      }
     }
   }
 })
+
+// date function
+function date() {
+  const currentDate = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+  const dateString = currentDate.toLocaleString('en-US', options);
+  document.querySelector(".date").innerHTML += dateString
+};
+date()
